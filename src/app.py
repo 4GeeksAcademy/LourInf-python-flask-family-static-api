85ebfd3a195692ee964c121cd7713b0a3cbfa847
Lourdes Infante <117685514+LourInf@users.noreply.github.com>
2023-12-13 22:35:53 +0000

    complete CRUD

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ import os
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from datastructures import FamilyStructure
+from datastructures import FamilyStructure #we need to import the data structure from the other file we have
 #from models import Person
 
 app = Flask(__name__)
@@ -33,12 +33,12 @@ def get_members():
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()  # this is the method for returning a list with all the family members
     response_body = {
-        "family": members # we defined the attribute members in datastructure.py file as an empty array so this is exactly what I would see in Postman. To see members in the list, we need to use the method POST
+        "results": members # we defined the attribute members in datastructure.py file as an empty array so this is exactly what I would see in Postman. To see members in the list, we need to use the method POST
     }
-    return jsonify(response_body), 200   # jsonify transforms our python code into JSON so our front can receive it. Tb enviamos el codigo 200 (ok)
+    return jsonify(response_body), 200   # jsonify transforms our python code into JSON so our front can receive it. We can receive 2 parameters in our return, so we also receive the success code 200 (ok)
 
 #### POST ####
-@app.route('/member', methods=['POST'])
+@app.route('/members', methods=['POST'])
 def handle_add_members():
     new_family_member = request.json # As we want to add 3 family members, then is best to send the request from Postman, writing the the dictionary (as the one below for kate) in the body, and receive that request here as json. We create for 1 member and post, another member and post, and so on.
 
@@ -50,9 +50,49 @@ def handle_add_members():
     
     members = jackson_family.add_member(new_family_member) # this is the method for adding a new family member to the list. 
     response_body = {
-        "family": members
+        "results": members
     }
     return jsonify(response_body), 200
+
+#### GET 1 ####
+@app.route('/members/<int:member_id>', methods=['GET'])
+def get_specific_member(member_id): #whatever we put as id after the endpoint we need to also pass it as a parameter here
+    single_member = jackson_family.get_member(member_id) # this is the method for getting a particular family member from the list. 
+    if single_member:
+        response_body = {"message": "family member found",
+                        "results": single_member }
+        return jsonify(response_body), 200
+    response_body = {"message": "family member not found",
+                            "results": [] }
+    return jsonify(response_body), 404
+    
+#### DELETE ####
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def handle_delete_member(member_id):   #whatever we put as id after the endpoint we need to also pass it as a parameter here   
+    deleted_member = jackson_family.delete_member(member_id) # this is the method for deleting a family member from the list. 
+    if deleted_member:
+        response_body = {"message": "family member deleted",
+                        "results": deleted_member }
+        return jsonify(response_body), 200
+    response_body = {"message": "family member not found",
+                    }
+    return jsonify(response_body), 404
+
+    
+#### PUT ####
+@app.route('/members/<int:member_id>', methods=['PUT'])
+def handle_update_member(member_id):   #whatever we put as id after the endpoint we need to also pass it as a parameter here   
+      # Get the updated member data from the request body
+    updated_member_data = request.json
+    updated_member = jackson_family.update_member(member_id, updated_member_data) # this is the method for updating a family member from the list. 
+    if updated_member:
+        response_body = {"message": "family member updated",
+                        "results": updated_member }
+        return jsonify(response_body), 200
+    response_body = {"message": "family member not found",
+                    }
+    return jsonify(response_body), 404
+# REMEMBER: When sending a PUT request with JSON data, make sure to set the in Postman headers the "Content-Type" (key) as "application/json" (value)!
 
 
 # this only runs if `$ python src/app.py` is executed
--- src/datastructures.py
+++ src/datastructures.py
@@ -11,29 +11,47 @@ from random import randint
 class FamilyStructure:  #1. we define our class which will represent the structure and behaviour of a family object
     def __init__(self, last_name): #2. we initialize it with the constructor method "__init__", like saying "to start creating a FamilyStructure we first need the FamilyStructure itself (self) and whatever else we want to add as a parameter (last_name)
         self.last_name = last_name #3.we define the attributes that my FamilyStructure will have, so I'm saying: this FamilyStructure has a last name of X (the last_name that I will pass). So this attribute will store the family's last name.
-        self._members = [] # this list stores family members
+        self._members = [] # this list stores family members (it's a list of dictionaries)
    
     #4. here we define the methods we want to create and use in our object
     # this method generates random members ID's when adding members into the list
-    def _generateId(self):
+    def _generate_id(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
         # To add the ID to a member, which are all dictionaries, we use this syntax: name_of_dictionary["key"] = value. And we use the above function generateId, to create a random Id. 
         # we also check if the member already has an id, because otherwise python gives us an error. To check if a value in a dictionary already exists we use the method .get()
         if not member.get("id"):
-            member["id"] = self._generateId() 
+            member["id"] = self._generate_id() 
         # to add to a list we use the .append method
         self._members.append(member)
 
+     # delete a single member by its id:
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == id:
+                self._members.remove(member) 
+                return True
+        return False  
 
+    
+    # this method gets a single member by its id
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == id:
+                return member
+        return False  
+    
 
     # this method returns a list with all the family members
     def get_all_members(self):
         return self._members
+
+     # this method updates a family member
+    def update_member(self, id, updated_member):
+        for index, member in enumerate(self._members): # to find an element AND its index in a list, we use method .enumerate(), which gets both the index and the element itself during each iteration
+            if member["id"] == id:
+                self._members[index] = updated_member # if the id matches, take that element with the index and replace it directly with the new info given to the updated_member
+                return True
+        return False
+
